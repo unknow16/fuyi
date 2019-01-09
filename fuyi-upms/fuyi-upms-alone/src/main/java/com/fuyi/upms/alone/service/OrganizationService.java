33e80ea5796b75b846d9331a5794f33604354bb4
fuyi <1406751952@qq.com>
2019-01-10 00:16:31 +0800

    fuyi-upms 结构优化

--- fuyi-upms/fuyi-upms-alone/src/main/java/com/fuyi/upms/alone/service/OrganizationService.java
+++
@@ -1,8 +0,0 @@
-package com.fuyi.upms.alone.service;
-
-import com.fuyi.framework.api.base.BaseService;
-import com.fuyi.upms.dao.entity.UpmsOrganization;
-import com.fuyi.upms.dao.entity.UpmsOrganizationExample;
-
-public interface OrganizationService extends BaseService<UpmsOrganization, UpmsOrganizationExample> {
-}
--- fuyi-upms/fuyi-upms-alone/src/main/java/com/fuyi/upms/alone/service/SystemService.java
+++
@@ -1,8 +0,0 @@
-package com.fuyi.upms.alone.service;
-
-import com.fuyi.framework.api.base.BaseService;
-import com.fuyi.upms.dao.entity.UpmsSystem;
-import com.fuyi.upms.dao.entity.UpmsSystemExample;
-
-public interface SystemService extends BaseService<UpmsSystem, UpmsSystemExample> {
-}
